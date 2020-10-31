bfaf893a5e5e722178a5ac775c61d310a7a6daeb
CG360 <CloudGamer360@users.noreply.github.com>
2020-10-31 22:11:42 +0000

    Register game (And copy old kit, it wasn't broken) - Changes sumo references to sumo x and remained compatible with old sumo maps.

--- src/main/java/net/mooncraftgames/mantle/gamemodesumox/SumoX.java
+++ src/main/java/net/mooncraftgames/mantle/gamemodesumox/SumoX.java
@@ -2,6 +2,14 @@ package net.mooncraftgames.mantle.gamemodesumox;
 
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.plugin.PluginLogger;
+import net.mooncraftgames.mantle.gamemodesumox.games.GBehaveSumoBase;
+import net.mooncraftgames.mantle.gamemodesumox.kits.KitSlapper;
+import net.mooncraftgames.mantle.newgamesapi.game.GameHandler;
+import net.mooncraftgames.mantle.newgamesapi.game.GameID;
+import net.mooncraftgames.mantle.newgamesapi.game.GameProperties;
+import net.mooncraftgames.mantle.newgamesapi.kits.KitGroup;
+import net.mooncraftgames.mantle.newgamesapi.registry.GameRegistry;
+import net.mooncraftgames.mantle.newgamesapi.registry.KitRegistry;
 
 public class SumoX extends PluginBase {
 
@@ -10,6 +18,19 @@ public class SumoX extends PluginBase {
     @Override
     public void onEnable() {
         sumoxinstance = this;
+
+        KitRegistry.get().registerKitGroup(new KitGroup("sumox", new KitSlapper()));
+
+        GameProperties sumoProperties = new GameProperties(GameHandler.AutomaticWinPolicy.MANUAL_CALLS_ONLY)
+                .setCanPlayersMoveDuringCountdown(false)
+                .setCanWorldBeManipulated(false)
+                .setDefaultCountdownLength(10)
+                .setMinimumPlayers(2)
+                .setGuidelinePlayers(6)
+                .setMaximumPlayers(16);
+        GameID sumoID = new GameID("sumox", "sumox", "Sumo X", "The all new Sumo! Slap players off the platform till they run out of lives! The last standing wins each round!", "sumox", new String[]{"sumo", "sumox"}, 2, sumoProperties, GBehaveSumoBase.class);
+
+        GameRegistry.get().registerGame(sumoID);
     }
 
     public static SumoX get() { return sumoxinstance; }
---
+++ src/main/java/net/mooncraftgames/mantle/gamemodesumox/kits/KitSlapper.java
@@ -0,0 +1,42 @@
+package net.mooncraftgames.mantle.gamemodesumox.kits;
+
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemFish;
+import cn.nukkit.item.ItemHelmetDiamond;
+import cn.nukkit.item.enchantment.Enchantment;
+import cn.nukkit.utils.TextFormat;
+import net.mooncraftgames.mantle.newgamesapi.kits.Kit;
+
+import java.util.Optional;
+
+public class KitSlapper extends Kit {
+
+    @Override
+    public String getKitID() {
+        return "slapper";
+    }
+
+    @Override
+    public String getKitDisplayName() {
+        return "Slapper";
+    }
+
+    @Override
+    public String getKitDescription() {
+        return "A basic kit, the slapper does more knockback than the others kits at the cost of no easy way to recover.";
+    }
+
+    @Override
+    public Item[] getKitItems() {
+        Item slapItem =  new ItemFish().setCustomName(""+TextFormat.GREEN+TextFormat.BOLD+"Ol' Slappy");
+        slapItem.addEnchantment(Enchantment.get(Enchantment.ID_PROTECTION_ALL).setLevel(1));
+        return new Item[]{
+           slapItem
+        };
+    }
+
+    @Override
+    public Optional<Item> getKitHelmet() {
+        return Optional.of(new ItemHelmetDiamond());
+    }
+}
