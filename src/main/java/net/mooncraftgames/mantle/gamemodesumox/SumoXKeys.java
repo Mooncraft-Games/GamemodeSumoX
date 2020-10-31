e8d15da7b558f0811e1b3356f87586ae9a36af81
CG360 <CloudGamer360@users.noreply.github.com>
2020-10-31 21:45:05 +0000

    Rename keys to remove SUMOX prefix.

--- src/main/java/net/mooncraftgames/mantle/gamemodesumox/SumoXKeys.java
+++ src/main/java/net/mooncraftgames/mantle/gamemodesumox/SumoXKeys.java
@@ -2,8 +2,8 @@ package net.mooncraftgames.mantle.gamemodesumox;
 
 public final class SumoXKeys {
 
-    public static final String SUMOX_INT_TIMER = "game_length";
-    public static final String SUMOX_INT_RESPAWN_SECS = "respawn_seconds";
-    public static final String SUMOX_INT_LIVES = "lives";
+    public static final String INT_TIMER = "game_length";
+    public static final String INT_RESPAWN_SECS = "respawn_seconds";
+    public static final String INT_LIVES = "lives";
 
 }
