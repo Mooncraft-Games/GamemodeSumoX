4701b6f2e8ed3619d3a4e2bcd66c0cdb6fe0fa50
CG360 <CloudGamer360@users.noreply.github.com>
2020-11-04 20:24:49 +0000

    Implemented Sound + GameHandler hooks to powerups

--- src/main/java/net/mooncraftgames/mantle/gamemodesumox/powerup/PowerUp.java
+++ src/main/java/net/mooncraftgames/mantle/gamemodesumox/powerup/PowerUp.java
@@ -1,13 +1,29 @@
 package net.mooncraftgames.mantle.gamemodesumox.powerup;
 
+import cn.nukkit.level.Sound;
+import net.mooncraftgames.mantle.newgamesapi.game.GameHandler;
+
 public abstract class PowerUp {
+
+    protected GameHandler gameHandler;
+
+    public PowerUp (GameHandler gameHandler){
+        this.gameHandler = gameHandler;
+    }
 
     public abstract String getName();
     public abstract String getDescription();
     public abstract String getUsage();
 
+    public abstract Sound useSound();
+    public float useSoundPitch() { return 1f; }
+    public float useSoundVolume() { return 0.5f; }
+
+    public abstract int getWeight();
+
     public abstract boolean isConsumedImmediatley();
 
     public abstract boolean use(PowerUpContext context);
+    public abstract void cleanUp();
 
 }
