c0e924ab9cf3a22c29073968c9ddd9d72c7848ce
CG360 <CloudGamer360@users.noreply.github.com>
2020-11-04 21:04:20 +0000

    Experimental PowerUp implementation

--- src/main/java/net/mooncraftgames/mantle/gamemodesumox/games/pointentities/PETypeSumoXPowerUpSpot.java
+++ src/main/java/net/mooncraftgames/mantle/gamemodesumox/games/pointentities/PETypeSumoXPowerUpSpot.java
@@ -1,6 +1,12 @@
 package net.mooncraftgames.mantle.gamemodesumox.games.pointentities;
 
+import cn.nukkit.Player;
 import cn.nukkit.entity.mob.EntityGuardian;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
+import cn.nukkit.event.HandlerList;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.level.Level;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.math.Vector2;
@@ -9,11 +15,14 @@ import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.nbt.tag.DoubleTag;
 import cn.nukkit.nbt.tag.FloatTag;
 import cn.nukkit.nbt.tag.ListTag;
+import dev.cg360.mc.nukkittables.types.entry.TableEntry;
 import net.mooncraftgames.mantle.gamemodesumox.SumoX;
 import net.mooncraftgames.mantle.gamemodesumox.SumoXConstants;
 import net.mooncraftgames.mantle.gamemodesumox.SumoXKeys;
 import net.mooncraftgames.mantle.gamemodesumox.SumoXStrings;
 import net.mooncraftgames.mantle.gamemodesumox.games.GBehaveSumoBase;
+import net.mooncraftgames.mantle.gamemodesumox.powerup.PowerUp;
+import net.mooncraftgames.mantle.gamemodesumox.powerup.PowerUpContext;
 import net.mooncraftgames.mantle.newgamesapi.game.GameHandler;
 import net.mooncraftgames.mantle.newgamesapi.map.pointentities.PointEntityCallData;
 import net.mooncraftgames.mantle.newgamesapi.map.pointentities.PointEntityType;
@@ -22,17 +31,45 @@ import net.mooncraftgames.mantle.newgamesapi.map.types.PointEntity;
 import java.util.Random;
 import java.util.UUID;
 
-public class PETypeSumoXPowerUpSpot extends PointEntityType {
+public class PETypeSumoXPowerUpSpot extends PointEntityType implements Listener {
 
     private boolean triggeredMisuseWarning = false;
 
+    private int maxWeight;
+    private PowerUp[] powerUpPool;
+
     public PETypeSumoXPowerUpSpot(GameHandler gameHandler) {
         super(SumoXKeys.PE_TYPE_POWERUP, gameHandler);
+
+        this.maxWeight = 0;
+        this.powerUpPool = new PowerUp[0];
     }
 
     @Override
     public void onRegister() {
         this.addFunction(SumoXKeys.PE_FUNC_POWERUP_SPAWN, this::spawnPowerUp);
+
+        this.powerUpPool = new PowerUp[SumoXConstants.AVAILABLE_POWER_UPS.size()];
+        for(int i = 0; i < SumoXConstants.AVAILABLE_POWER_UPS.size(); i++){
+            try {
+                Class<? extends PowerUp> c = SumoXConstants.AVAILABLE_POWER_UPS.get(i);
+                PowerUp powerUp = c.newInstance();
+                powerUpPool[i] = powerUp;
+                maxWeight += Math.max(powerUp.getWeight(), 1);
+            } catch (Exception err){
+                this.maxWeight = 0;
+                this.powerUpPool = new PowerUp[0];
+                SumoX.getPlgLogger().error("Very Broken PowerUp: Constructor Fault.");
+                return;
+            }
+        }
+
+        SumoX.get().getServer().getPluginManager().registerEvents(this, SumoX.get());
+    }
+
+    @Override
+    public void onUnregister() {
+        HandlerList.unregisterAll(this);
     }
 
     protected void spawnPowerUp(PointEntityCallData data){
@@ -56,9 +93,7 @@ public class PETypeSumoXPowerUpSpot extends PointEntityType {
                     spawnPowerUpEntity(pe).spawnToAll();
                 }
             } else {
-                Random r = new Random();
-                int calcTime = behaviours.getMinimumPowerUpSpawnTime() + r.nextInt(behaviours.getVariationPowerUpSpawnTime());
-                behaviours.getPowerUpPointCooldowns().put(pe.getId(), calcTime);
+                behaviours.getPowerUpPointCooldowns().put(pe.getId(), generateNewTime(behaviours));
             }
         } else {
             if(!triggeredMisuseWarning) {
@@ -100,6 +135,63 @@ public class PETypeSumoXPowerUpSpot extends PointEntityType {
         guardian.setScale(2);
 
         return guardian;
+    }
+
+    public int generateNewTime(GBehaveSumoBase behaviours){
+        Random r = new Random();
+        return behaviours.getMinimumPowerUpSpawnTime() + r.nextInt(behaviours.getVariationPowerUpSpawnTime());
+    }
+
+    @EventHandler
+    protected boolean runPowerUp(PowerUp powerUp, PowerUpContext context){
+        if(powerUp.isConsumedImmediatley()){
+            boolean result = powerUp.use(context);
+            if(result){
+                //TODO: Message
+                context.getPlayer().getLevel().addSound(
+                        context.getPlayer().getPosition(),
+                        powerUp.useSound(),
+                        powerUp.useSoundVolume(),
+                        powerUp.useSoundPitch(),
+                        context.getPlayer());
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            //TODO: Item Usage.
+        }
+        return false;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onEntityHit(EntityDamageByEntityEvent event){
+        if(event.getDamager() instanceof Player){
+            Player attacker = (Player) event.getDamager();
+
+            if(getGameHandler().getPlayers().contains(attacker)){
+
+                if(event.getEntity().namedTag.contains(SumoXKeys.NBT_POWERUP_PE_TIE)){
+
+                    String s = event.getEntity().namedTag.getString(SumoXKeys.NBT_POWERUP_PE_TIE);
+                    if(s != null){
+                        if(getGameHandler().getGameBehaviors() instanceof GBehaveSumoBase) {
+                            int selection = new Random().nextInt(maxWeight);
+                            int cumulativeWeightChecked = 1;
+                            for(PowerUp entry: powerUpPool){
+                                if(selection <= (cumulativeWeightChecked + entry.getWeight())){
+                                    runPowerUp(entry, new PowerUpContext(attacker));
+                                }
+                                cumulativeWeightChecked += entry.getWeight();
+                            }
+
+                            GBehaveSumoBase behaviours = (GBehaveSumoBase) getGameHandler().getGameBehaviors();
+                            behaviours.getPowerUpPointCooldowns().put(s, generateNewTime(behaviours));
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
